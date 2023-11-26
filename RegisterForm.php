35ed9b217b90b85d15869ada16c21f829dee1160
SUBASRIBALRAJ <118466095+SUBASRIBALRAJ@users.noreply.github.com>
2023-11-26 12:18:13 +0530

    Create RegisterForm.php

---
+++ RegisterForm.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+</head>
+<body>
+    <div class ="container">
+    <form action="Registration.php" method="post">
+    <div class ="form-group">
+        <input type="text" name="fullname" placeholder ="Full Name">
+        <div class ="form-group">
+        <input type="text" name="email" placeholder ="Email">
+   
+        <div class ="form-group">
+        <input type="text" name="password" placeholder ="Password">
+   
+        <div class ="form-group">
+        <input type="text" name="repeat-password" placeholder ="Repeat Password">
+        
+
+    <div class="form-group">
+        <input type="submit" value="Register" name="submit">
+</div>
+</form> 
+
+       
+
+
+   
+
+</body>
+</html>
